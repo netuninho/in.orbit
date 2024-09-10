b37462dd23a43d6ea78f2debedcf6abebe038c29
Manu <manuelass.dev@gmail.com>
2024-09-10 20:07:36 -0300

    first commit

---
+++ server/src/db/seed.ts
@@ -0,0 +1,25 @@
+import dayjs from "dayjs"
+import { client, db } from "."
+import { goalCompletions, goals } from "./schema"
+
+async function seed() {
+    await db.delete(goalCompletions)
+    await db.delete(goals)
+
+    const result = await db.insert(goals).values([
+        { title: 'Acordar cedo', desireWeeklyFrequency: 5 },
+        { title: 'Yoga', desireWeeklyFrequency: 3 },
+        { title: 'Estudar', desireWeeklyFrequency: 3 }
+    ]).returning()
+
+    const startOfWeek = dayjs().startOf('week')
+
+    await db.insert(goalCompletions).values([
+        { goalId: result[0].id, createdAt: startOfWeek.toDate() },
+        { goalId: result[2].id, createdAt: startOfWeek.add(1, 'day').toDate() },
+    ])
+}
+
+seed().finally(() => {
+    client.end()
+})
