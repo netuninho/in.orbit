5150b02450ec7ae4abaa09ff1a05dbf8855ab76d
Manu <manuelass.dev@gmail.com>
2024-09-10 22:57:29 -0300

    feat: rota para criacao de goals feita

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -4,7 +4,7 @@ import { pgTable, text, integer, timestamp } from 'drizzle-orm/pg-core';
 export const goals = pgTable('goals', {
     id: text('id').primaryKey().$defaultFn(() => createId()),
     title: text('title').notNull(),
-    desireWeeklyFrequency: integer('desired_weekly_frequency').notNull(),
+    desiredWeeklyFrequency: integer('desired_weekly_frequency').notNull(),
     createdAt: timestamp('created_at', { withTimezone: true }).notNull().defaultNow(),
 })
 
